4f4aefc273334e33bcd1c20bcd2d4537803f558e
Biviana Agudelo <biviana_agudelo@hotmail.com>
2024-06-13 15:21:19 -0500

    Se agregan estilos a los Tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,7 +1,49 @@
-const Tag = () => {
-    return(
-        <p>Tags</p>
-    )
-}
+import styled from "styled-components";
+import tags from "./tags.json";
 
-export default Tag
+const TagsContainer = styled.section`
+    display: flex;
+    align-items: center;
+    gap: 64px;
+    margin-top: 56px;
+`
+
+const TagTitulo = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin: 0;
+`;
+
+const TagBTN = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
+
+const Div = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: end;
+`;
+
+const Tag = () => {
+    return (
+        <TagsContainer>
+            <TagTitulo>Busque por tags:</TagTitulo>
+            <Div>
+                {tags.map((tag) => (<TagBTN key={tag.id}>{tag.titulo}</TagBTN>))}
+            </Div>
+        </TagsContainer>
+    );
+};
+
+export default Tag;
